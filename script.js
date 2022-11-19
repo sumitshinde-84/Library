f3be52522e6abffad0f81413767da2488184dd0a
sumitshinde-84 <shindesumit217@gmail.com>
2022-11-19 22:15:32 +0530

    logic for add books added

--- script.js
+++ script.js
@@ -1,38 +1,76 @@
-
-
 let myLibrary = [];
-let book;
-
-
+let  book;
+let count = 0;
+let i =0;
+let bookinfo;
+let bookDiv;
 const addBook = document.querySelector('#addBook')
-const bookTitle = document.querySelector('#title');
-const bookAuthor = document.querySelector('#author');
-const bookPages = document.querySelector('#pages');
+const bookTitle = document.querySelector('#bookTitle');
+const bookAuthor = document.querySelector('#bookAuthor');
+const bookCode = document.querySelector('#book-code')
+const bookPages = document.querySelector('#bookPages');
 const submit = document.querySelector('#submit');
-
-
-function Book(title,author,pages){
-
-this.title = title;
-this.author = author;
-this.pages = pages;
-    
-}
-
-
-function addBookToLibrary(){
-
-
-
- book = new Book(bookTitle.value,bookAuthor.value,bookPages.value)
-    
-
-}
+const add = document.querySelector('#add');
+const form = document.querySelector('form')
+const formAdd = document.querySelector('.form-add')
+let rakk = document.querySelector('.rakk0')
 
 
 
 
-submit.addEventListener('click',(book)=>{
-    myLibrary.push(book);
-    console.log(myLibrary)
+
+
+
+add.addEventListener('click',()=>{
+
+form.style.top="20px"
 })
+
+formAdd.addEventListener('click',()=>{
+    form.style.top="-600px"
+
+})
+
+
+
+formAdd.addEventListener('click',()=>{
+    
+    if(count<=9){
+        rakk = document.querySelector(`.rakk${i}`)
+        count++;
+       
+        
+    }else{
+        count=0
+        i++;
+        rakk = document.querySelector(`rakk${i}`)
+    }
+
+     book = Object()
+     book.title = bookTitle.value ;
+     book.author = bookAuthor.value;
+     book.pages = bookPages.value;
+     book.code = bookCode.value;
+     bookDiv =document.createElement('div');
+     bookDiv.className='book';
+     bookDiv.setAttribute('id',`rakk${i}${count}`)
+     rakk.appendChild(bookDiv);
+     myLibrary.push(book);
+     console.log(myLibrary)
+    
+     
+     
+    
+})
+
+
+
+for(i=0;i<myLibrary.length;i++){
+for(j=count;j<9;j++){
+     bookinfo = document.querySelector(`#rakk${i}${j}`)
+    bookinfo.addEventListener('click',()=>{
+        console.log(myLibrary[i].title)
+    })
+}
+
+}
