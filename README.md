7290fd3c7f030f4f4c0e2900cef751e2a1076031
sumitshinde-84 <shindesumit217@gmail.com>
2022-11-20 19:21:59 +0530

    display,remove,read status logic added

--- README.md
+++ README.md
@@ -2,3 +2,24 @@
 
 
 
+// function remove(){
+
+    
+    
+
+//     codeForRemove=BookNo.textContent
+//     codeForRemove = codeForRemove.charAt(codeForRemove.length - 1)
+//     console.log(codeForRemove)
+    
+//    delete  myLibrary[ codeForRemove - 1 ]
+//    let divforRemove = document.querySelector(`.book${codeForRemove}`) 
+//     rakk.removeChild(divforRemove)
+ 
+
+
+// }
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let myLibrary = [];
-let  book;
+let book;
 let count = 0;
-let i =0;
-let bookinfo;
+let i = 0;
+let j = 0;
 let bookDiv;
+let bookCount =0;
+let totalRead = 0;
+
 const addBook = document.querySelector('#addBook')
 const bookTitle = document.querySelector('#bookTitle');
 const bookAuthor = document.querySelector('#bookAuthor');
@@ -14,63 +17,147 @@ const add = document.querySelector('#add');
 const form = document.querySelector('form')
 const formAdd = document.querySelector('.form-add')
 let rakk = document.querySelector('.rakk0')
+const paraTitle = document.querySelector('.paraTitle')
+const paraAuthor = document.querySelector('.paraAuthor')
+const paraPages = document.querySelector('.paraPages');
+const paraStatus = document.querySelector('.paraStatus');
+const BookNo = document.querySelector('.BookNo')
+const circle = document.querySelector('.circle')
+const togle = document.querySelector('.togle')
+const Total = document.querySelector('.total')
+const ReadCount = document.querySelector('.totalRead');
+let checkbox=false;
+const Status = document.querySelector('.Status')
+const togleSwitch = document.querySelector('.switch')
+const checkBox = document.querySelector('.checkBox')
 
 
 
 
+ function forToggleSwitch(){
+
+    if( Status.textContent=='Not Read'){
+        Status.textContent='Read'
+        totalRead++;
+        ReadCount.textContent=`Total Read :${totalRead}`;
+       
+        }
+  
+        else if(Status.textContent=='Read'){
+            Status.textContent='Not Read'
+            totalRead--;
+            ReadCount.textContent=`Total Read :${totalRead}`;
+            
+            }
+           
+        }
 
 
 
-add.addEventListener('click',()=>{
 
-form.style.top="20px"
+   
+
+
+
+add.addEventListener('click', () => {
+
+    form.style.top = "20px"
 })
 
-formAdd.addEventListener('click',()=>{
-    form.style.top="-600px"
+formAdd.addEventListener('click', () => {
+    form.style.top = "-600px"
 
 })
 
 
 
-formAdd.addEventListener('click',()=>{
-    
-    if(count<=9){
+formAdd.addEventListener('click', () => {
+
+    if (count < 10) {
         rakk = document.querySelector(`.rakk${i}`)
         count++;
-       
-        
-    }else{
-        count=0
+        j++;
+        if( Status.textContent=='Not Read'){
+            if(totalRead<=0){
+                totalRead = 0;
+
+            }else{
+                totalRead--;
+            }
+            
+            ReadCount.textContent=`Total Read :${totalRead}`;
+           
+            }
+    } else {
+
         i++;
         rakk = document.querySelector(`rakk${i}`)
+        count = 0
+
+
     }
 
-     book = Object()
-     book.title = bookTitle.value ;
-     book.author = bookAuthor.value;
-     book.pages = bookPages.value;
-     book.code = bookCode.value;
-     bookDiv =document.createElement('div');
-     bookDiv.className='book';
-     bookDiv.setAttribute('id',`rakk${i}${count}`)
-     rakk.appendChild(bookDiv);
-     myLibrary.push(book);
-     console.log(myLibrary)
-    
-     
-     
-    
+    book = Object()
+    book.title = bookTitle.value;
+    book.author = bookAuthor.value;
+    book.pages = bookPages.value;
+    book.status=Status.textContent;
+    console.log(book.status)
+    book.code = bookCode.value;
+    bookDiv = document.createElement('div');
+    codeNo = document.createElement('p');
+    codeNo.textContent =`${j}`
+    bookDiv.appendChild(codeNo);
+    bookDiv.setAttribute(`class`,`book book${j}`);
+    bookDiv.setAttribute('onClick', `display(${j})`)
+    rakk.appendChild(bookDiv);
+    bookCount++;
+    Total.textContent=`Total books: ${bookCount}`;
+    myLibrary.push(book);
+    Status.textContent='Not Read'
+
+
+
 })
 
 
 
-for(i=0;i<myLibrary.length;i++){
-for(j=count;j<9;j++){
-     bookinfo = document.querySelector(`#rakk${i}${j}`)
-    bookinfo.addEventListener('click',()=>{
-        console.log(myLibrary[i].title)
+
+
+function display(j) {
+
+    let div = document.querySelector(`.book${j}`)
+    div.addEventListener('click',()=>{
+
+        let para = div.querySelector('p')
+        let code = para.textContent;
+        paraTitle.textContent = `Title : ${myLibrary[code - 1].title}`;
+        paraAuthor.textContent = `Author : ${myLibrary[code - 1].author}`;
+        paraPages.textContent = `Pages : ${myLibrary[code - 1].pages}`;
+        BookNo.textContent = `Book No : ${code}`;
+        paraStatus.textContent=`Status :${myLibrary[code - 1].status}`
+        
     })
+
+
 }
+
+
+
+
+
+function remove(){
+
+    
+    
+
+    codeForRemove=BookNo.textContent
+    codeForRemove = codeForRemove.charAt(codeForRemove.length - 1)
+    console.log(codeForRemove)
+   delete  myLibrary[ codeForRemove - 1 ]
+   let divforRemove = document.querySelector(`.book${codeForRemove}`) 
+    rakk.removeChild(divforRemove)
+ 
+
 
 }
